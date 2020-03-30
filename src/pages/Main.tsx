a5d031a6f3e1b2ce32236fd490f08afb08679c7e
Kenton Lam <kenton_lam@outlook.com>
2020-03-30 19:21:10 +1000

    Spacing digits.

--- src/pages/Main.tsx
+++ src/pages/Main.tsx
@@ -2,7 +2,14 @@ import { StorageProps, useStorage } from "../services/storage";
 import React from "react";
 import { format } from "date-fns";
 
-const renderHours = (n: number, useColour?: boolean) => {
+const smallHours = (n: number) => {
+  const suffix = n === 1 ? '' : 's';
+  return <span>
+    <b>{n < 10 ? <>&#8199;</> : ''}{n}</b> hour{suffix}
+  </span>;
+}
+
+const largeHours = (n: number, useColour?: boolean) => {
   let colour;
   if (n === 0)
     colour = 'green';
@@ -30,15 +37,13 @@ export const Main = () => {
       <div className="block">
         <div className="is-size-4">{format(new Date(), NICE_FORMAT)}</div>
         {/* style={{backgroundColor: '#363636', color: 'white'}} */}
-        <span className="is-size-2">You are behind {renderHours(10, true)}!</span>
+        <span className="is-size-2">You are behind {largeHours(10, true)}!</span>
       </div>
-      <div className="columns is-gapless">
-        <div className="column is-narrow" style={{marginRight: '0.8ex'}}>Which is made up of</div>
-         <div className="column">
-           {renderHours(2)} of CSSE1001, and<br></br>
-           {renderHours(1)} of STAT3004.</div>
-
-      </div>
+      Which is made up of...<br></br>
+      {smallHours(20)} of CSSE1001, and<br></br>
+      {smallHours(2)} of CSSE1001, and<br></br>
+      {smallHours(15)} of CSSE1001, and<br></br>
+           {smallHours(1)} of STAT3004.
     </div>
   </div>
 };
